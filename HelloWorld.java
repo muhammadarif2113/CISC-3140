22d3e7ba309b13d7e7a72ae3b086d52df858f00b
Muhammad Arif <arifmuhammad2113@gmail.com>
2021-03-20 22:36:42 -0400

    HelloWorld.java has been updated with todays date

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld{
 
 public static void main (String [] args){ 
-	hello(); 
+	hello();  
+   System.out.println("Todays date is March 20, 2021"); 
 }
 public static String hello (){
 	System.out.println("Hello World");
