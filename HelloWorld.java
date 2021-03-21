0671b0004499d622c5195e9e4c0165afeee08185
Muhammad Arif <arifmuhammad2113@gmail.com>
2021-03-20 22:03:51 -0400

    HelloWorld.java - output is HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+
+public static void main (String [] args){ 
+	hello(); 
+}
+public static String hello (){
+	System.out.println("Hello World");
+	return "Hello World";  
+}
+}
